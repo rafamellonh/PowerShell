abf2e0205fede53c5ebc43dded6ae73f47aa90ca
rafael.mello <56116868+rafamellonh@users.noreply.github.com>
2023-01-19 10:37:21 -0300

    PS

--- Veeam.ps1
+++ Veeam.ps1
@@ -1,4 +1,3 @@
 # Captura os backups de 2023 e com status failed
-
-$erro = get-VBRBackupSession | Where-Object {$_.EndTime -like "*2023*"} | Where-Object {$_.Result -eq "Failed"}
+# $erro = get-VBRBackupSession | Where-Object {$_.EndTime -like "*2023*"} | Where-Object {$_.Result -eq "Failed"}
 
