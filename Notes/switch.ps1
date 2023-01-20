24c91da60786e5e444b7746dafe73dbbc2226ea7
rafael.mello <56116868+rafamellonh@users.noreply.github.com>
2023-01-20 14:39:37 -0300

    PS

--- Notes/switch.ps1
+++ Notes/switch.ps1
@@ -7,3 +7,17 @@ switch ($cor) {
     "amarelho" {"você escolheu : "}
     Default {"branco"}
 }
+
+
+$dia = (Get-Date).DayOfWeek.value__
+
+switch ($dia) {
+    0 {"Domingo"; break}
+    1 {"Segunda"; break}
+    2 {"Terça"; break}
+    3 {"Quarta"; break}
+    4 {"Quinta"; break}
+    5 {"Sexta"; break}
+    6 {"Sabado"; break}
+    Default {}
+}
