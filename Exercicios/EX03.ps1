6f9e8e342c90713bd34fad41aa3360daaf4e09db
rafael.mello <56116868+rafamellonh@users.noreply.github.com>
2023-01-21 13:44:00 -0300

    PS

---
+++ Exercicios/EX03.ps1
@@ -0,0 +1,7 @@
+$nome = Read-Host "Digite seu nome"
+$idade = Read-Host "Digite sua idade"
+$peso = Read-Host "Digite seu peso"
+$altura = Read-Host "Digite sua altura"
+
+"`tNome`t `tIdade`t `tPeso`t `tAltura`t"
+"`t$nome`t `t$idade`t `t$peso`t `t$altura`t"
