874edbc7f144ae6eff79a66edc7e1d7fbc01e6e6
rafael.mello <56116868+rafamellonh@users.noreply.github.com>
2023-01-23 16:50:39 -0300

    PS

---
+++ Notes/Matrizes.ps1
@@ -0,0 +1,24 @@
+$x = 12
+$y = 88
+$z = 59
+
+#Na matriz
+$array = 12, 88, 59
+$array
+
+foreach ($n in $array) {
+    $n * $n
+}
+
+$matriz = 1..10
+foreach ($n in $matriz) {
+    "o número é " + $n
+}
+
+$matriz[3] #mostra o que tem na posição 3 da matriz
+
+@matriz1 = @(3)
+
+#mostra somente o que tem dentro de cada casa na matriz
+$num = 1..20
+$num[2,4,5,13]
