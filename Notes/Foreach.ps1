d4bcf218bb9d010d5373659a6221d3fb4a2cc50e
rafael.mello <56116868+rafamellonh@users.noreply.github.com>
2023-01-23 16:28:01 -0300

    PS

--- Notes/Foreach.ps1
+++ Notes/Foreach.ps1
@@ -14,3 +14,17 @@ foreach ($cor in [Enum]::GetValues([System.ConsoleColor])){Write-Host "      " -
 ForEach($arq in Get-ChildItem c:\Windows\System32 -File){
     if($arq.length -gt 1mb){$arq.Name}
 }
+
+#Pegar todos os arquivos de um formato e colocar em uma pasta com a data de criação
+#seta a origem e destino
+$path = "D:\teste"
+Set-Location D:\Wallpaper
+#Se não existir o destino, vai ser criado
+if (!(Test-Path $path)) {
+    New-Item -Path $path -ItemType Directory
+}
+
+# para cada item dentro de d:\wallpaper, copie para pasta da variavel $path (get-childitem só funciona pq foi setado a pasta antes com set-location)
+foreach ($item in Get-ChildItem -Filter *.jpg -Recurse)  {
+    Copy-Item $item.FullName $path
+}
