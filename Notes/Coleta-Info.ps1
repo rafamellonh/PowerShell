78937d6026d05356081c8831ee99c0791f843791
rafael.mello <56116868+rafamellonh@users.noreply.github.com>
2023-02-08 16:24:26 -0300

    PowerShell

--- Notes/Coleta-Info.ps1
+++ Notes/Coleta-Info.ps1
@@ -11,3 +11,15 @@ Get-Counter -ListSet *memory* | where countersetname -eq "memory" | select -Expa
 
 
 Get-Counter -ListSet *memory* | where countersetname -eq "GPU Process Memory" | select -ExpandProperty paths
+
+#Obtem todas as classes do sistema
+Get-CimClass
+Get-CimClass -ClassName *memory*  # somente classes da memória
+
+#Obtem com o GetwmiObject
+Get-WmiObject -Class Win32_PhysicalMemory | select -Property BankLabel,Capacity
+Get-WmiObject -Class Win32_LogicalDisk
+Get-WmiObject -Class win32_bios
+
+# Pode ser utilizado também o get-ciminstance que o resultado será o mesmo
+Get-CimInstance -ClassName win32_bios
