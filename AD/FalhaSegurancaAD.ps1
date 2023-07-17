2c4a32fdf514db617dd351517f1483a1120a3c2c
Rafael <56116868+rafamellonh@users.noreply.github.com>
2023-07-17 13:18:21 -0300

    Update FalhaSegurancaAD.ps1

--- AD/FalhaSegurancaAD.ps1
+++ AD/FalhaSegurancaAD.ps1
@@ -1,1 +1,4 @@
+#Execute como admin local
 Test-ComputerSecureChannel -repair -credential
+
+# https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.management/test-computersecurechannel?view=powershell-5.1
