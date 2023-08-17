aa1e3bfa99dce92889bceb0237eb27baba658c4f
Rafael <56116868+rafamellonh@users.noreply.github.com>
2023-08-17 12:33:13 -0300

    Create Sufixo-Azure-AD.ps1

---
+++ Azure/Sufixo-Azure-AD.ps1
@@ -0,0 +1,17 @@
+import-module activedirectory
+
+#Old domain suffix
+$oldSuffix = "mello.com.br"
+
+#New domain suffix
+$newSuffix = "rafamelloaz86outlook.onmicrosoft.com"
+
+#Specify the OU this script will target
+$ou = “OU=Departamentos,DC=tfteccompany,DC=com,DC=br”
+
+#Specify a writeable domain controller
+$server = “VM-DC-PRD”
+
+Get-ADUser -SearchBase $ou -filter * | ForEach-Object {
+$newUpn = $_.UserPrincipalName.Replace($oldSuffix,$newSuffix)
+$_ | Set-ADUser -server $server -UserPrincipalName $newUpn
