2785d7ae070bbae3bca4570cc80a2bd633d13d7c
Rafael <56116868+rafamellonh@users.noreply.github.com>
2024-02-07 16:33:37 -0500

    Update infra.ps1

--- Scripts/WinScp/infra.ps1
+++ Scripts/WinScp/infra.ps1
@@ -1,1 +1,7 @@
+New-Item -ItemType Directory -Path (Join-Path -Path "C:\" -ChildPath "files")
+New-Item -ItemType Directory -Path (Join-Path -Path "C:\files" -ChildPath "logs")
+New-Item -ItemType Directory -Path (Join-Path -Path "C:\files" -ChildPath "scripts")
+New-Item -ItemType Directory -Path (Join-Path -Path "C:\files" -ChildPath "temp")
 
+
+me
