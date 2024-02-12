1295faa687f94c7a16bd1a5cfa5e812fbd47a1ec
Rafael <56116868+rafamellonh@users.noreply.github.com>
2024-02-12 09:14:54 -0500

    Update WinSCP.ps1

--- Scripts/WinScp/WinSCP.ps1
+++ Scripts/WinScp/WinSCP.ps1
@@ -1,5 +1,5 @@
 # Maximum number of attempts
-$maxTries = 3
+$maxTries = 5
 
 # Initialize the attempt counter
 $tryCount = 0
@@ -14,37 +14,40 @@ while ($tryCount -lt $maxTries) {
 & "C:\Users\rafael.demello\AppData\Local\Programs\WinSCP\WinSCP.com" `
   /log="C:\files\logs\WinSCP.log" /ini=nul `
   /command `
-    "open sftp://sftp:abc%2C123@dbei7nift3jko.azurecontainer.io/ -hostkey=`"`"ssh-ed25519 255 bR2VgdfgdfgdgL7zZMfB1kMLrSnaFxGrPZqLOhkkEgg`"`"" `
-    "put -nopreservetime ""C:\files\temp*"" /upload/01/" `
+  "open sftp://sftp:abc%2C123@dbei7nift3jko.canadaeast.azurecontainer.io/ -hostkey=`"`"ssh-ed25519 255 2lGodF/0wSHrSuVlMaM19otNNuMcvOFlksNhqJ5PoJc`"`"" `
+  "put -nopreservetime ""C:\files\temp"" /upload/01/" `
     "exit"
 
 # Get the result from WinSCP
 $winscpResult = $LastExitCode
 
 # Get weekDay
-$DayOfWeek = (Get-Date).DayOfWeek
-
+#$DayOfWeek = (Get-Date).DayOfWeek
+$date = Get-Date -Format "dddd yyyy/MM/dd HH:mm"
 
 # Checks whether the operation was successful
     if ($winscpResult -eq 0) {
-        Add-Content -Path "C:\files\logs\TransferToHQ-Result.txt" -Value "$DayOfWeek $((Get-Date).ToString()) Success"
+        #Add-Content -Path "C:\files\logs\TransferToHQ-Result.txt" -Value "$DayOfWeek $((Get-Date).ToString()) Success"
+        Add-Content -Path "C:\files\logs\TransferToHQ-Result.txt" -Value "$date Success"
+        Add-Content -Path "C:\files\logs\tryCount.txt" -Value "Number of attempts : $tryCount"
         & "C:\files\scripts\SendEmail-v2.ps1"
         break  # Exit the loop if successful
 
     } else {
-        Add-Content -Path "C:\files\logs\TransferToHQ-Result.txt" -Value "$DayOfWeek $((Get-Date).ToString()) Error"
+        #Add-Content -Path "C:\files\logs\TransferToHQ-Result.txt" -Value "$DayOfWeek $((Get-Date).ToString()) Error"
+        Add-Content -Path "C:\files\logs\TransferToHQ-Result.txt" -Value "$date Error"
         Start-Sleep -Seconds 5  # Wait 5 seconds before trying again
     }
 }
 
 if ($winscpResult -ne 0) {
-    Write-Host "Número máximo de tentativas atingido. Falha na execução."
+    Add-Content -Path "C:\files\logs\tryCount.txt" -Value "Number of attempts : $tryCount"
     & "C:\files\scripts\SendEmail-v2.ps1"
 }
 
 # Exit with WinSCP exit code
 #exit $winscpResult
 
-
+Write-Host $tryCount
 
 
