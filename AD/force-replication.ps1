43d6d7538b18c1889ef605790ad342922e190d2c
Rafael <56116868+rafamellonh@users.noreply.github.com>
2025-01-26 11:03:18 -0500

    Create force-replication.ps1

---
+++ AD/force-replication.ps1
@@ -0,0 +1,11 @@
+repadmin /syncall /e /A /P /d /q
+
+repadmin /syncall /AdeP
+
+# Aqui cada um dos parâmetros explicados:
+# /syncall: Sincroniza o domain controller principal com todos os parceiros de replicação;
+# /e: Enterprise… indica que a replicação será estendida a todos os sites remotos;
+# /A:  All; Sincroniza todas as partições do home server;
+# /P: Indica que as mudanças serão replicadas a partir do servidor primário;
+# /d: Identifica os servidores por seus nomes distintos em eventuais mensagens que possam aparecer;
+# /q: Quiet mode. Não mostra as mensagens de status. Caso você esteja fazendo isso manualmente por algum problema, é melhor não usar este parâmetro para identificar algum erro.
